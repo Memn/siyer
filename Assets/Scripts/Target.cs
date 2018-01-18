82de3655bd34bf99679cc56262d60357674ebd56
Memn <m.e.mumcu@gmail.com>
2018-01-18 16:07:36 +0300

    * random target positioning after target hit

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    float xMin = -45.0f;
+    float xMax = 45.0f;
+    float zMin = -270.0f; // -300-270 = 30 meters far
+    float zMax = -50.0f; //  -300-50  = 250 meters far
 
     public void SetPostion()
     {
-        print("we will set position");
+        transform.position = new Vector3(Random.Range(xMin, xMax), transform.position.y, Random.Range(zMin, zMax));
     }
 
 }
