812ad2ca366e206afd5b02bec98466d8203864a8
Memn <m.e.mumcu@gmail.com>
2018-01-23 19:00:41 +0300

    - First Q&A Template (Fill the blanks) - Drag&Drop System

---
+++ Assets/Scripts/questionanswer/QuestionBody.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class QuestionBody : MonoBehaviour, IDropHandler
+{
+    void IDropHandler.OnDrop(PointerEventData eventData)
+    {
+        print("change the text with: ");
+		GetComponent<Text>().text = Choice.itemBeingDragged.GetComponent<Text>().text;
+
+    }
+}
