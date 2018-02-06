471b8ea622cef6ad66b2a32ef9df4688ba272245
Memn <m.e.mumcu@gmail.com>
2018-02-06 18:11:36 +0300

    - Splash screen added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public float loadMainMenuAfter;
+    
+
+    void Start()
+    {
+        if (loadMainMenuAfter > 0)
+            Invoke("LoadMainMenu", loadMainMenuAfter);
+    }
+
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("01 MainMenu");
+    }
+
+    public void QuitRequest()
+    {
+        Application.Quit();
+
+    }
+
+
+}
