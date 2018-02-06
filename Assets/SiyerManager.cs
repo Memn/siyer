680e8a79e7f7e8b777d2b24970d4b823d3b9d16f
Memn <m.e.mumcu@gmail.com>
2018-02-06 18:51:38 +0300

    - Fade in

---
+++ Assets/SiyerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SiyerManager : MonoBehaviour
+{
+
+    public GameObject fadeInPanel;
+
+    void Awake()
+    {
+        fadeInPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("01a MainMenu");
+        }
+
+    }
+}
