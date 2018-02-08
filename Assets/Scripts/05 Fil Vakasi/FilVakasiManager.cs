54c1d1ec3e3f6caf81e0a6ea3fbf6ec91436c348
Memn <m.e.mumcu@gmail.com>
2018-02-08 11:15:50 +0300

    - Fil vakasi geri tusu eklendi.

---
+++ Assets/Scripts/05 Fil Vakasi/FilVakasiManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class FilVakasiManager : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("02 Siyer");
+        }
+	}
+}
