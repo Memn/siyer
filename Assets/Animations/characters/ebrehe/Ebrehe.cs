7d83edfc55e0b0796bfac06b4442ead939320b5a
Memn <m.e.mumcu@gmail.com>
2018-02-14 17:53:14 +0300

    * ilk sesli animasyon denemesi

---
+++ Assets/Animations/characters/ebrehe/Ebrehe.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ebrehe : MonoBehaviour
+{
+
+    private AudioSource source;
+
+    private int sceneNumber = 0;
+    private Animator animator;
+    public AudioClip[] clips;
+
+    public FilVakasiManager manager;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void SceneEnter()
+    {
+        sceneNumber = animator.GetInteger("scene number");
+        EnterScene(sceneNumber);
+    }
+
+    void EnterScene(int scene)
+    {
+        switch (scene)
+        {
+            case 1:
+                ShowingDede();
+                Audio(scene);
+                Invoke("ShowingKaaba", 3.6f);
+                Invoke("Angry", 7);
+                Invoke("Laugh", 13.75f);
+                Invoke("SceneExit", 17);
+                break;
+            case 2:
+                Audio(scene);
+                Laugh();
+                Invoke("Mocking", 0.7f);
+                Invoke("Angry", 5);
+                Invoke("SceneExit", 15.5f);
+                break;
+            case 3:
+                Angry();
+                Invoke("ShowingKaaba", 0.5f);
+                Audio(scene);
+                Invoke("Speak", 5.5f);
+                Invoke("SceneExit", 6.0f);
+                break;
+            default:
+                throw new NotImplementedException("Scene number for Ebrehe is not valid: " + scene);
+        }
+    }
+
+    private void Audio(int scene)
+    {
+        source.PlayOneShot(clips[scene - 1]);
+    }
+
+    void ShowingDede()
+    {
+        animator.SetTrigger("showing-dede");
+    }
+    void ShowingKaaba()
+    {
+        animator.SetTrigger("showing-kaaba");
+    }
+
+    void Speak()
+    {
+        animator.SetTrigger("speak");
+    }
+    void Angry()
+    {
+        animator.SetTrigger("angry");
+    }
+
+    void Laugh()
+    {
+        animator.SetTrigger("laugh");
+    }
+    void Mocking()
+    {
+        animator.SetTrigger("mocking");
+    }
+    void SceneExit()
+    {
+        animator.SetTrigger("scene exit");
+        manager.EbreheExit();
+    }
+
+}
--- Assets/Scripts/05 Fil Vakasi/FilVakasiManager.cs
+++ Assets/Scripts/05 Fil Vakasi/FilVakasiManager.cs
@@ -1,15 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class FilVakasiManager : MonoBehaviour {
+public class FilVakasiManager : MonoBehaviour
+{
 
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Escape))
+    public Animator dede;
+    public Animator ebrehe;
+
+    private int scene = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("StartScene", 2);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             SceneManager.LoadScene("02 Siyer");
         }
-	}
+    }
+
+    void StartScene()
+    {
+        ebrehe.SetInteger("scene number", scene);
+        ebrehe.SetTrigger("scene enter");
+    }
+
+    internal void EbreheExit()
+    {
+        dede.SetInteger("scene number", scene);
+        dede.SetTrigger("scene enter");
+    }
+
+    internal void DedeExit()
+    {
+        scene++;
+        if (scene < 4)
+            StartScene();
+    }
 }
