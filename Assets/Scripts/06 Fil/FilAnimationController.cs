f91d1df3621da92c491794e4ab562530cc61dd38
Memn <m.e.mumcu@gmail.com>
2018-02-24 16:31:08 +0300

    - Fil vakasi animasyon guncellemeleri

---
+++ Assets/Scripts/06 Fil/FilAnimationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilAnimationController : MonoBehaviour
+{
+
+    private int scene = 1;
+    private AudioSource source;
+    public AudioClip[] ebreheClips;
+
+    public AudioClip[] dedeClips;
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        // idle movements can be done here?
+    }
+
+    void EbrehePlay(int index)
+    {
+        source.PlayOneShot(ebreheClips[index]);
+    }
+
+
+
+}
