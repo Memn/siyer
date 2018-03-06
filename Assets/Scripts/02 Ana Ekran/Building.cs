0c8512330ef1266b90db50a71377e91af5d5dac5
Memn <m.e.mumcu@gmail.com>
2018-03-06 14:25:39 +0300

    * Ana Ekran Bina Secimi

---
+++ Assets/Scripts/02 Ana Ekran/Building.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Building : MonoBehaviour
+{
+    private SpriteRenderer _sprite;
+
+    public SceneManagementUtil.Scenes Scene;
+    public bool Selected;
+
+    private void Start()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+
+    public void GoScene()
+    {
+        SceneManagementUtil.Load(Scene);
+    }
+
+    public void Unselect()
+    {
+        _sprite.color = Color.white;
+        Selected = false;
+    }
+
+    public void Select()
+    {
+        _sprite.color = Color.yellow;
+        Selected = true;
+    }
+}
---
+++ Assets/Scripts/02 Ana Ekran/BuildingManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    public Building[] Buildings;
+
+    public void Select(GameObject b)
+    {
+        var building = FindInBuildings(b);
+        if (!building)
+        {
+            UnselectAll();
+            return;
+        }
+
+        if (building.Selected)
+        {
+            building.GoScene();
+        }
+        else
+        {
+            UnselectAll();
+            building.Select();
+        }
+    }
+
+    private void UnselectAll()
+    {
+        foreach (var building1 in Buildings)
+        {
+            building1.Unselect();
+        }
+    }
+
+    private Building FindInBuildings(GameObject b)
+    {
+        if (!b.GetComponent<Building>()) return null;
+        var buildingName = b.name;
+        return Buildings.FirstOrDefault(building => building.name == buildingName);
+    }
+}
