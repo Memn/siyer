0439d8d7ab43562c599f734ecd2c39de8d1c1c67
Memn <m.e.mumcu@gmail.com>
2018-03-06 14:28:16 +0300

    - WordGame Library is imported

---
+++ Assets/Third Party/Word Game/Assets/Scripts/Map.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Map : MonoBehaviour {
+    public GameObject hexPrefab;
+    
+    int width = 5, height=5;
+    public static string word = "Kainatboşlukkabuletmezabii";
+    public static string imgDir = "Assets/Resources/Letters/";
+    public static string Empty = imgDir + "Empty.png";
+    void Start () {
+
+        Puzzle puzzle = PuzzleMaker.MakePuzzle(word);
+        int index = 0;
+        Vector2 pos=new Vector2(0,0);
+        Vector2 start = new Vector2(-1*puzzle.width/2, -1*puzzle.height/2);
+        for (int x = 0; x < puzzle.width; x++)
+        {
+            for (int y = 0; y < puzzle.height; y++)
+            {
+             
+                if (y % 2 == 0)
+                {
+                    pos = new Vector2(start.x+ x + 0.5f, start.y+ y);
+                }
+                else
+                {
+                    pos = new Vector2(start.x + x , start.y + y);
+                }
+
+                string img2 = imgDir + puzzle.puzzleData[x,y]+".png";
+                GameObject go = Instantiate(hexPrefab, pos, Quaternion.identity) ;
+                go.GetComponent<SpriteRenderer>().sprite = LoadNewSprite(img2);
+                go.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                index++;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Sprite LoadNewSprite(string FilePath, float PixelsPerUnit = 100.0f)
+    {
+
+        // Load a PNG or JPG image from disk to a Texture2D, assign this texture to a new sprite and return its reference
+
+        Sprite NewSprite = new Sprite();
+        Texture2D SpriteTexture = LoadTexture(FilePath);
+        NewSprite = Sprite.Create(SpriteTexture, new Rect(0, 0, SpriteTexture.width, SpriteTexture.height), new Vector2(0, 0), PixelsPerUnit);
+
+        return NewSprite;
+    }
+
+    public Texture2D LoadTexture(string FilePath)
+    {
+
+        // Load a PNG or JPG file from disk to a Texture2D
+        // Returns null if load fails
+
+        Texture2D Tex2D=null;
+        byte[] FileData;
+
+        if (File.Exists(FilePath))
+        {
+            FileData = File.ReadAllBytes(FilePath);
+            
+             // If data = readable -> return texture
+        }
+        else
+        {
+            FileData = File.ReadAllBytes(Empty);
+        }
+        Tex2D = new Texture2D(2, 2);           // Create new "empty" texture
+        if (Tex2D.LoadImage(FileData))
+        {
+            //Tex2D.Resize(Tex2D.width / 2, Tex2D.height / 2);
+            return Tex2D;
+        }// Load the imagedata into the texture (size is set automatically)
+        return null;                     // Return null if load failed
+    }
+}
---
+++ Assets/Third Party/Word Game/Assets/Scripts/PuzzleMaker.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+public struct Position
+{
+    public int x;
+    public int y;
+    public Position(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+public struct Puzzle
+{
+    public int width;
+    public int height;
+    public char[,] puzzleData;
+    public string word;
+    public Puzzle(string word)
+    {
+        this.word = word;
+        int size = (int)(Math.Round(Math.Sqrt(word.Length)));
+        if (size * size < word.Length)
+        {
+            this.height = size + 1;
+        }
+        else
+        {
+            this.height = size;
+        }
+        
+        this.width = size;
+        puzzleData = new char[ width, height];
+        Array.Clear(puzzleData, 0, height * width);
+    }
+}
+public class PuzzleMaker : MonoBehaviour {
+
+    static Position UP = new Position(-1, 0);
+    static Position UPL = new Position(-1, -1);
+    static Position RIGHT = new Position(0, 1);
+    static Position DOWNL = new Position(1, -1);
+    static Position DOWN = new Position(1, 0);
+    static Position LEFT = new Position(0, -1);
+    //static Position[] SIDES = { UP, UPL,  RIGHT, DOWNL, DOWN, LEFT };
+    static Position[] SIDES = { UP, RIGHT, DOWN, LEFT };
+
+    //public static string word = "Siyer, İslam dini literatüründe peygamberlerin, din büyüklerinin ve halifelerin hayat hikâyesidir.";
+
+    public static Puzzle MakePuzzle(string word)
+    {
+        
+        Puzzle puzzle = new Puzzle(word);
+
+        //Position position = new Position(randomNumber.Next(size-1), randomNumber.Next(size-1) );
+        Position position = new Position(0, 0);
+        
+        addNewCharToPuzzle(0, position, puzzle);
+        printPuzzle(puzzle);
+        return puzzle;
+    }
+
+    public static bool addNewCharToPuzzle(int charIndex, Position position, Puzzle puzzle)
+    {
+
+        short sideNumber;
+        int tryNumber = 0;
+        bool added = false;
+        if (puzzle.word.Length <= charIndex)
+        {
+            added = true;
+        }
+        else if (position.x >= 0 && position.x < puzzle.width && position.y >= 0 && position.y < puzzle.height && puzzle.puzzleData[position.x, position.y] == 0)
+        {
+            puzzle.puzzleData[position.x, position.y] = puzzle.word.ElementAt(charIndex);
+
+            sideNumber = (short)UnityEngine.Random.Range(1,SIDES.Length);
+            // sideNumber = 0;
+            while (!added && tryNumber < SIDES.Length)
+            {
+                tryNumber++;
+                Position nextPosition = new Position((SIDES[sideNumber].x + position.x), (SIDES[sideNumber].y + position.y));
+                sideNumber = (short)((sideNumber + 1) % SIDES.Length);
+                added = addNewCharToPuzzle(charIndex + 1, nextPosition, puzzle);
+            }
+            if (!added)
+            {
+                puzzle.puzzleData[position.x, position.y] = (char)0;
+            }
+
+
+        }
+
+
+        return added;
+    }
+
+    public static void printPuzzle(Puzzle puzzle)
+    {
+        for (int i = 0; i < puzzle.width; i++)
+        {
+            for (int j = 0; j < puzzle.height; j++)
+            {
+                Console.Write(puzzle.puzzleData[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
