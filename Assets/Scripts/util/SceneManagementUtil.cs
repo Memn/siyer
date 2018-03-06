f955fcee51f29b553bc39e6cf6802557e729b0fd
Memn <m.e.mumcu@gmail.com>
2018-03-06 14:27:46 +0300

    - Kutuphane Scene is added & Scene folder renamed

--- Assets/Scripts/util/SceneManagementUtil.cs
+++ Assets/Scripts/util/SceneManagementUtil.cs
@@ -7,12 +7,13 @@ public class SceneManagementUtil : MonoBehaviour
     public enum Scenes
     {
         MainMenu = 1,
-        Profile=2,
-        AnaEkran =3,
+        Profile = 2,
+        AnaEkran = 3,
         Talimhane = 4,
         SoruCevap = 5,
         FilVakasi = 6,
         Labirent = 7,
+        Kutuphane = 8
     };
 
     public static void Load(Scenes scene)
