66d3734ad117957c4d836c55a85273f75d403949
Memn <m.e.mumcu@gmail.com>
2018-03-20 17:41:42 +0300

    * Talimhane yeni hedef ve ok vurus kalibrasyon guncellemeleri

--- Assets/Scripts/03 Talimhane/FPArcherController.cs
+++ Assets/Scripts/03 Talimhane/FPArcherController.cs
@@ -6,50 +6,35 @@ using UnityStandardAssets.Utility;
 [RequireComponent(typeof(AudioSource))]
 public class FPArcherController : MonoBehaviour
 {
-    private bool m_IsWalking = false;
-    [SerializeField] private float m_WalkSpeed = 5;
-    [SerializeField] private float m_RunSpeed = 10;
-    [SerializeField] [Range(0f, 1f)] private float m_RunstepLenghten;
-    private float m_GravityMultiplier = 2;
-    [SerializeField] private ArcherMouseLook m_MouseLook;
-    [SerializeField] private bool m_UseFovKick;
-    [SerializeField] private FOVKick m_FovKick = new FOVKick();
-    [SerializeField] private bool m_UseHeadBob;
-    [SerializeField] private CurveControlledBob m_HeadBob = new CurveControlledBob();
-    [SerializeField] private LerpControlledBob m_JumpBob = new LerpControlledBob();
-    [SerializeField] private float m_StepInterval;
+    private bool _mIsWalking = false;
+    [SerializeField] [Range(0f, 1f)] private float _runstepLenghten;
+    private float _gravityMultiplier = 2;
+    [SerializeField] private FOVKick _fovKick = new FOVKick();
+    [SerializeField] private CurveControlledBob _headBob = new CurveControlledBob();
+    [SerializeField] private float _stepInterval;
 
-    [SerializeField]
-    private AudioClip[] m_FootstepSounds; // an array of footstep sounds that will be randomly selected from.
+    [SerializeField] private AudioClip _breathSound; // the sound played when character leaves the ground.
+    [SerializeField] private AudioClip _landSound; // the sound played when character touches back on ground.
 
-    [SerializeField] private AudioClip m_BreathSound; // the sound played when character leaves the ground.
-    [SerializeField] private AudioClip m_LandSound; // the sound played when character touches back on ground.
-
-    private Camera m_Camera;
+    private Camera _camera;
     private bool _hold;
-    private float m_YRotation;
-    private Vector2 m_Input;
-    private Vector3 m_MoveDir = Vector3.zero;
-    private CharacterController m_CharacterController;
-    private CollisionFlags m_CollisionFlags;
-    private bool m_PreviouslyGrounded;
-    private Vector3 m_OriginalCameraPosition;
-    private float m_StepCycle;
-    private float m_NextStep;
-    private AudioSource m_AudioSource;
+    private float _yRotation;
+    private Vector2 _input;
+    private CharacterController _characterController;
+    private bool _previouslyGrounded;
+    private AudioSource _audioSource;
+    private ArcherMouseLook _mouseLook;
 
     // Use this for initialization
     private void Start()
     {
-        m_CharacterController = GetComponent<CharacterController>();
-        m_Camera = Camera.main;
-        m_OriginalCameraPosition = m_Camera.transform.localPosition;
-        m_FovKick.Setup(m_Camera);
-        m_HeadBob.Setup(m_Camera, m_StepInterval);
-        m_StepCycle = 0f;
-        m_NextStep = m_StepCycle / 2f;
-        m_AudioSource = GetComponent<AudioSource>();
-        m_MouseLook.Init(transform, m_Camera.transform);
+        _characterController = GetComponent<CharacterController>();
+        _camera = Camera.main;
+        _fovKick.Setup(_camera);
+        _headBob.Setup(_camera, _stepInterval);
+        _audioSource = GetComponent<AudioSource>();
+        _mouseLook = GetComponent<ArcherMouseLook>();
+        _mouseLook.Init(transform, _camera.transform);
     }
 
 
@@ -59,205 +44,71 @@ public class FPArcherController : MonoBehaviour
         RotateView();
         if (CrossPlatformInputManager.GetButtonDown("Jump"))
         {
-            m_MouseLook.Holding = true;
+            _mouseLook.Holding = true;
             if (!_hold)
             {
                 _hold = true;
-                m_AudioSource.loop = true;
+                _audioSource.loop = true;
                 PlayHoldSound();
             }
         }
 
         if (CrossPlatformInputManager.GetButtonUp("Jump"))
         {
-            m_MouseLook.Holding = false;
+            _mouseLook.Holding = false;
             if (_hold)
             {
-                m_AudioSource.loop = false;
-                m_AudioSource.Stop();
+                _audioSource.loop = false;
+                _audioSource.Stop();
                 _hold = false;
             }
         }
 
 
-        if (!m_PreviouslyGrounded && m_CharacterController.isGrounded)
+        if (!_previouslyGrounded && _characterController.isGrounded)
         {
             PlayLandingSound();
-            m_MoveDir.y = 0f;
         }
 
-        if (!m_CharacterController.isGrounded && m_PreviouslyGrounded)
-        {
-            m_MoveDir.y = 0f;
-        }
-
-        m_PreviouslyGrounded = m_CharacterController.isGrounded;
+        _previouslyGrounded = _characterController.isGrounded;
     }
 
 
     private void PlayLandingSound()
     {
-        m_AudioSource.clip = m_LandSound;
-        m_AudioSource.Play();
-        m_NextStep = m_StepCycle + .5f;
+        _audioSource.clip = _landSound;
+        _audioSource.Play();
     }
 
 
     private void FixedUpdate()
     {
-        float speed;
-        GetInput(out speed);
-        // always move along the camera forward as it is the direction that it being aimed at
-        Vector3 desiredMove = transform.forward * m_Input.y + transform.right * m_Input.x;
-
-        // get a normal for the surface that is being touched to move along it
-        RaycastHit hitInfo;
-        Physics.SphereCast(transform.position, m_CharacterController.radius, Vector3.down, out hitInfo,
-            m_CharacterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
-        desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
-
-        m_MoveDir.x = desiredMove.x * speed;
-        m_MoveDir.z = desiredMove.z * speed;
-
-
-        if (!m_CharacterController.isGrounded)
-        {
-            m_MoveDir += Physics.gravity * m_GravityMultiplier * Time.fixedDeltaTime;
-        }
-
-        m_CollisionFlags = m_CharacterController.Move(m_MoveDir * Time.fixedDeltaTime);
-
-        ProgressStepCycle(speed);
-        UpdateCameraPosition(speed);
-
-        m_MouseLook.UpdateCursorLock();
+        _mouseLook.UpdateCursorLock();
     }
 
 
     private void PlayHoldSound()
     {
-        m_AudioSource.clip = m_BreathSound;
-        m_AudioSource.Play();
+        _audioSource.clip = _breathSound;
+        _audioSource.Play();
     }
-
-
-    private void ProgressStepCycle(float speed)
-    {
-        if (m_CharacterController.velocity.sqrMagnitude > 0 && (m_Input.x != 0 || m_Input.y != 0))
-        {
-            m_StepCycle +=
-                (m_CharacterController.velocity.magnitude + (speed * (m_IsWalking ? 1f : m_RunstepLenghten))) *
-                Time.fixedDeltaTime;
-        }
-
-        if (!(m_StepCycle > m_NextStep))
-        {
-            return;
-        }
-
-        m_NextStep = m_StepCycle + m_StepInterval;
-
-        PlayFootStepAudio();
-    }
-
-
-    private void PlayFootStepAudio()
-    {
-        if (!m_CharacterController.isGrounded)
-        {
-            return;
-        }
-
-        // pick & play a random footstep sound from the array,
-        // excluding sound at index 0
-        int n = Random.Range(1, m_FootstepSounds.Length);
-        m_AudioSource.clip = m_FootstepSounds[n];
-        m_AudioSource.PlayOneShot(m_AudioSource.clip);
-        // move picked sound to index 0 so it's not picked next time
-        m_FootstepSounds[n] = m_FootstepSounds[0];
-        m_FootstepSounds[0] = m_AudioSource.clip;
-    }
-
-
-    private void UpdateCameraPosition(float speed)
-    {
-        Vector3 newCameraPosition;
-        if (!m_UseHeadBob)
-        {
-            return;
-        }
-
-        if (m_CharacterController.velocity.magnitude > 0 && m_CharacterController.isGrounded)
-        {
-            m_Camera.transform.localPosition =
-                m_HeadBob.DoHeadBob(m_CharacterController.velocity.magnitude +
-                                    (speed * (m_IsWalking ? 1f : m_RunstepLenghten)));
-            newCameraPosition = m_Camera.transform.localPosition;
-            newCameraPosition.y = m_Camera.transform.localPosition.y - m_JumpBob.Offset();
-        }
-        else
-        {
-            newCameraPosition = m_Camera.transform.localPosition;
-            newCameraPosition.y = m_OriginalCameraPosition.y - m_JumpBob.Offset();
-        }
-
-        m_Camera.transform.localPosition = newCameraPosition;
-    }
-
-
-    private void GetInput(out float speed)
-    {
-        // Read input
-        float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
-        float vertical = CrossPlatformInputManager.GetAxis("Vertical");
-
-        bool waswalking = m_IsWalking;
-
-#if !MOBILE_INPUT
-// On standalone builds, walk/run speed is modified by a key press.
-// keep track of whether or not the character is walking or running
-            m_IsWalking = !Input.GetKey(KeyCode.LeftShift);
-#endif
-        // set the desired speed to be walking or running
-        speed = m_IsWalking ? m_WalkSpeed : m_RunSpeed;
-        m_Input = new Vector2(horizontal, vertical);
-
-        // normalize input if it exceeds 1 in combined length:
-        if (m_Input.sqrMagnitude > 1)
-        {
-            m_Input.Normalize();
-        }
-
-        // handle speed change to give an fov kick
-        // only if the player is going to a run, is running and the fovkick is to be used
-        if (m_IsWalking != waswalking && m_UseFovKick && m_CharacterController.velocity.sqrMagnitude > 0)
-        {
-            StopAllCoroutines();
-            StartCoroutine(!m_IsWalking ? m_FovKick.FOVKickUp() : m_FovKick.FOVKickDown());
-        }
-    }
-
 
     private void RotateView()
     {
-        m_MouseLook.LookRotation(transform, m_Camera.transform);
+        _mouseLook.LookRotation(transform, _camera.transform);
     }
 
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        Rigidbody body = hit.collider.attachedRigidbody;
+        var body = hit.collider.attachedRigidbody;
         //dont move the rigidbody if the character is on top of it
-        if (m_CollisionFlags == CollisionFlags.Below)
-        {
-            return;
-        }
 
         if (body == null || body.isKinematic)
         {
             return;
         }
 
-        body.AddForceAtPosition(m_CharacterController.velocity * 0.1f, hit.point, ForceMode.Impulse);
+        body.AddForceAtPosition(_characterController.velocity * 0.1f, hit.point, ForceMode.Impulse);
     }
 }
